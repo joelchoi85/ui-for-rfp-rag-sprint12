6c60c33997d7703adb274c2dc4c8a4c4a6f408bd
joelchoi85 <jymania@naver.com>
2026-09-02 17:45:12 +0900

    chore: eslint 버전 맞추기

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,5 @@
-export function proxy(req: Request) {
+export function proxy() {
+  // 되살릴 때 매개변수부터: export function proxy(req: Request)
   // const auth = req.headers.get("authorization");
   // const want = "Basic " + btoa(process.env.BASIC_AUTH!);
   // if (auth !== want)
